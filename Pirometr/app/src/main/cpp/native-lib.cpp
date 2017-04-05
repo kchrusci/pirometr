a120c0d0c8aa96f85dcf74fad86b2d297ce8e602
kchrusci <kchrusci@student.agh.edu.pl>
2017-04-05 22:17:45 +0200

    Created OpenCV project in Android Studio

--- Pirometr/app/src/main/cpp/native-lib.cpp
+++ Pirometr/app/src/main/cpp/native-lib.cpp
@@ -1,3 +1,11 @@
 #include <jni.h>
 #include <string>
 
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_android_pirometr_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Pirometr";
+    return env->NewStringUTF(hello.c_str());
+}

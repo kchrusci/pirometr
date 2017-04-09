bd988af9aa55eedebb82d867d84c8abb0221bece
kchrusci <kchrusci@student.agh.edu.pl>
2017-04-09 20:25:23 +0200

    Working basic Camera2 API example with OpenCv

--- Pirometr/app/src/main/cpp/native-lib.cpp
+++ Pirometr/app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,12 @@
-#include <jni.h>
+/*#include <jni.h>
 #include <string>
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_android_pirometr_MainActivity_stringFromJNI(
         JNIEnv *env,
-        jobject /* this */) {
+        jobject *//* this *//*) {
     std::string hello = "Pirometr";
     return env->NewStringUTF(hello.c_str());
-}
+}*/
+
--- Pirometr/app/src/main/java/com/android/pirometr/MainActivity.java
+++ Pirometr/app/src/main/java/com/android/pirometr/MainActivity.java
@@ -1,77 +1,20 @@
 package com.android.pirometr;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import org.opencv.android.OpenCVLoader;
+//import org.opencv.android.OpenCVLoader;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-        if(!OpenCVLoader.initDebug()) {
-            Log.d(TAG, "OpenCV not loaded");
-        } else {
-            Log.d(TAG, "OpenCV loaded");
-        }
-    }
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
         }
-
-        return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
